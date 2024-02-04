4362071a654b842a54830aab8d643683107d6887
Marco de Wild <marco.dewild@rovict.nl>
2024-02-04 22:00:19 +0100

    Migrations for order table

--- Yakify.Repository/Migrations/20240204210005_Orders.Designer.cs
+++ Yakify.Repository/Migrations/20240204210005_Orders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yakify.Repository;
 
@@ -11,9 +12,11 @@ using Yakify.Repository;
 namespace Yakify.Repository.Migrations
 {
     [DbContext(typeof(YakifyDbContext))]
-    partial class YakifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240204210005_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Yakify.Repository/Migrations/20240204210005_Orders.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yakify.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Orders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Customer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Milk = table.Column<double>(type: "float", nullable: true),
+                    Skins = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
