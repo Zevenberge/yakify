622e223e7cce00aad44871826770a52212cb0c69
Marco de Wild <marco.dewild@rovict.nl>
2024-02-05 22:17:22 +0100

    Resolve warning

--- Yakify.Api/Program.cs
+++ Yakify.Api/Program.cs
@@ -2,6 +2,10 @@ namespace Yakify.Api;
 
 public class Program
 {
+    protected Program()
+    {
+    }
+
     private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
