fd5c2d523c4cc1db0d4fa4605d3ab999e60d87fe
Marco de Wild <marco.dewild@rovict.nl>
2024-02-05 22:13:59 +0100

    Add day column to order table

--- Yakify.Repository/Migrations/20240205211230_Order_Add_day.Designer.cs
+++ Yakify.Repository/Migrations/20240205211230_Order_Add_day.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yakify.Repository;
 
@@ -11,9 +12,11 @@ using Yakify.Repository;
 namespace Yakify.Repository.Migrations
 {
     [DbContext(typeof(YakifyDbContext))]
-    partial class YakifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240205211230_Order_Add_day")]
+    partial class Order_Add_day
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
