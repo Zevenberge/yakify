6d15495fc075ec42d1f43a9a1834681381601354
Marco de Wild <marco.dewild@rovict.nl>
2024-02-06 21:50:55 +0100

    Beautified the stock card

--- yakify.web/src/Order.tsx
+++ yakify.web/src/Order.tsx
@@ -1,12 +1,15 @@
 import { GetResult } from "./hooks/useGet";
 import { StockDto, useStock } from "./hooks/useStock";
+import "./Order.css";
 
 export default function Order() {
   const stock = useStock();
 
-  return <>
-    <Stock stock={stock}/>
-  </>
+  return (
+    <>
+      <Stock stock={stock} />
+    </>
+  );
 }
 
 function Stock(props: { stock: GetResult<StockDto> }) {
@@ -17,18 +20,20 @@ function Stock(props: { stock: GetResult<StockDto> }) {
       </StockCard>
     );
   }
-  if(!props.stock.success) {
-    return <StockCard>
+  if (!props.stock.success) {
+    return (
+      <StockCard>
         <p>The yaks ran away! An error occured.</p>
-    </StockCard>
+      </StockCard>
+    );
   }
   return (
     <StockCard>
       <dl className="Order-list">
         <dt>Milk</dt>
-        <dd>{props.stock.result.milk}</dd>
-        <dt>Milk</dt>
-        <dd>{props.stock.result.milk}</dd>
+        <dd>{props.stock.result.milk.toFixed()}</dd>
+        <dt>Skins</dt>
+        <dd>{props.stock.result.skins.toFixed()}</dd>
       </dl>
     </StockCard>
   );
